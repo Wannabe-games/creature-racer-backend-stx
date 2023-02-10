3c5a61fdc3dbcd6c9358b026a98c82621926b062
Robert Pawłowski <robert@telemetric.pl>
2023-02-10 15:54:44 +0100

    Fix zakladania usera

--- src/admin/src/Controller/Admin/UserCrudController.php
+++ src/admin/src/Controller/Admin/UserCrudController.php
@@ -3,17 +3,26 @@
 namespace App\Controller\Admin;
 
 use App\Admin\Field\RolesField;
+use App\Common\Service\User\Utils\PasswordUpdaterInterface;
 use App\Entity\User;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private PasswordUpdaterInterface $passwordUpdater
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -25,14 +34,28 @@ class UserCrudController extends AbstractCrudController
             IdField::new('id')->onlyOnIndex(),
             RolesField::new('roles')->setPermission(User::ROLE_SUPER_ADMIN),
             TextField::new('nick'),
-            TextField::new('email'),
-            TextField::new('wallet'),
+            EmailField::new('email'),
+            TextField::new('plainPassword')->setLabel('Password'),
+            TextField::new('nick'),
             NumberField::new('player.gold')->setLabel('Gold')->hideWhenCreating(),
             NumberField::new('player.stacks')->setLabel('Stacks')->onlyOnIndex(),
             AssociationField::new('creatures'),
             DateTimeField::new('lastLogin')->onlyOnIndex(),
             DateTimeField::new('createdAt')->onlyOnIndex(),
-            BooleanField::new('enabled'),
+            BooleanField::new('enabled')->hideOnForm(),
         ];
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        /** @var User $entityInstance */
+        $entityInstance->setUsername($entityInstance->getEmail());
+        $entityInstance->setUsernameCanonical($entityInstance->getEmail());
+        $entityInstance->setEmailCanonical($entityInstance->getEmail());
+        $entityInstance->setEnabled(true);
+        $entityInstance->setCreatedAt(new DateTime());
+        $this->passwordUpdater->hashPassword($entityInstance);
+
+        parent::persistEntity($entityManager, $entityInstance);
     }
 }
